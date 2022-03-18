f235440be748b3322569ab32f60f7c1876fdefbb
Yukun Xia <yukunx@andrew.cmu.edu>
2022-03-18 17:05:10 -0400

    feat: test point cloud transformation in farmland_mapping pkg

--- src/farmland_mapping/src/farmland_mapping_node.cpp
+++ src/farmland_mapping/src/farmland_mapping_node.cpp
@@ -1,19 +1,45 @@
+#include <eigen_conversions/eigen_msg.h>
 #include <gazebo_msgs/ModelState.h>
 #include <gazebo_msgs/ModelStates.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <tf2/LinearMath/Transform.h>
+#include <tf2_eigen/tf2_eigen.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
 
+#include <Eigen/Dense>
 #include <pcl/PCLPointCloud2.h>
+#include <pcl/common/transforms.h>
 #include <pcl/conversions.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl/common/common.h>
+
+#include <iostream>
 
 nav_msgs::OccupancyGrid occupancy_grid;
+// occupancy_matrix has the same data as occupancy grid
+Eigen::Map<
+    Eigen::Matrix<int8_t, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>
+    occupancy_matrix(nullptr, 0, 0);
 bool robot_state_inited = false;
 int robot_state_index = -1;
 float robot_height = -1.0f;
+float robot_mobility_max_height = -1.0f;
+// robot_state.pose -> geometry_msgs::Pose
 gazebo_msgs::ModelState robot_state;
+// tf2::Transform base_link__tf2__velodyne;
+Eigen::Isometry3d base_link__eigenT__velodyne;
+Eigen::Isometry3d world__eigenT__velodyne;
+
+/*
+  Initialization stage functions
+*/
 
 bool initOccupancyGrid() {
   if (!ros::param::has("/world_size"))
@@ -54,58 +80,82 @@ bool initOccupancyGrid() {
   occupancy_grid.data = std::vector<int8_t>();
   occupancy_grid.data.resize(
       occupancy_grid.info.width * occupancy_grid.info.height, -1);
+  // occupancy_matrix.rows() == height (y range in gazebo world)
+  // cols() == width (x range in gazebo world)
+  // occupancy_matrix((y_w - y_omap) / res, (x_w - x_omap) / res)
+  new (&occupancy_matrix) Eigen::Map<
+      Eigen::Matrix<int8_t, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(
+      occupancy_grid.data.data(), occupancy_grid.info.height,
+      occupancy_grid.info.width);
 
   return true;
 }
 
-// bool initRobotState() {
-//   if (!ros::param::has("/robot_init_x"))
-//     return false;
-//   float robot_init_x = -1.0;
-//   ros::param::get("/robot_init_x", robot_init_x);
-//   assert(robot_init_x > 0.0f);
+bool initRobotTF() {
+  tf2_ros::Buffer tf_buffer;
+  tf2_ros::TransformListener tf_listener(tf_buffer);
 
-//   if (!ros::param::has("/robot_init_y"))
-//     return false;
-//   float robot_init_y = -1.0;
-//   ros::param::get("/robot_init_y", robot_init_y);
-//   assert(robot_init_y > 0.0f);
+  // base_link is the root link of the robot
+  geometry_msgs::TransformStamped base_link__geo_stamped_tf__velodyne;
+  try {
+    base_link__geo_stamped_tf__velodyne = tf_buffer.lookupTransform(
+        "base_link", "velodyne", ros::Time(0), ros::Duration(60.0));
+  } catch (tf2::TransformException &e) {
+    ROS_ERROR("tf2::TransformException error message: %s", e.what());
+    return false;
+  }
 
-//   if (!ros::param::has("/robot_init_z"))
-//     return false;
-//   float robot_init_z = -1.0;
-//   ros::param::get("/robot_init_z", robot_init_z);
-//   assert(robot_init_z > 0.0f);
+  // tf2::fromMsg(base_link__geo_stamped_tf__velodyne.transform,
+  //              base_link__tf2__velodyne);
+  base_link__eigenT__velodyne =
+      tf2::transformToEigen(base_link__geo_stamped_tf__velodyne);
 
-//   if (!ros::param::has("/robot_init_yaw"))
-//     return false;
-//   float robot_init_yaw = -1.0;
-//   ros::param::get("/robot_init_yaw", robot_init_yaw);
-//   assert(robot_init_yaw > 0.0f);
+  return true;
+}
 
-//   robot_state.pose.position.x = robot_init_x;
-//   robot_state.pose.position.y = robot_init_y;
-//   robot_state.pose.position.z = robot_init_z;
+/*
+  Helper functions
+*/
 
-//   return true;
-// }
+void mapPointCloudToCostmap(
+    const pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud) {
+
+  pcl::PassThrough<pcl::PointXYZ> point_cloud_height_filter;
+  point_cloud_height_filter.setInputCloud(point_cloud);
+  point_cloud_height_filter.setFilterFieldName("z");
+  point_cloud_height_filter.setFilterLimits(robot_mobility_max_height,
+                                            robot_height);
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud_blocking(
+      new pcl::PointCloud<pcl::PointXYZ>);
+  point_cloud_height_filter.filter(*point_cloud_blocking);
+}
+
+/*
+  Callback functions
+*/
 
 void velodynePointCloudCallback(
     const sensor_msgs::PointCloud2ConstPtr point_cloud_msg) {
   if (!robot_state_inited)
     return;
-
   pcl::PCLPointCloud2 pcl_pointcloud2_intermediate;
-  pcl::PointCloud<pcl::PointXYZ> pcl_point_cloud;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_point_cloud(
+      new pcl::PointCloud<pcl::PointXYZ>);
 
   pcl_conversions::toPCL(*point_cloud_msg, pcl_pointcloud2_intermediate);
-  pcl::fromPCLPointCloud2(pcl_pointcloud2_intermediate, pcl_point_cloud);
+  pcl::fromPCLPointCloud2(pcl_pointcloud2_intermediate, *pcl_point_cloud);
+  // source: in velodyne coords; target: in world coords
+  pcl::transformPointCloud(*pcl_point_cloud, *pcl_point_cloud,
+                           world__eigenT__velodyne.matrix());
 
   // TODO: ues point cloud and robot pose to update occupancy grid
+  mapPointCloudToCostmap(pcl_point_cloud);
 }
 
 // TODO: Use localization method to replace the gazebo magic
 void modelStatesCallback(const gazebo_msgs::ModelStatesConstPtr msg) {
+  // if not inited from gazebo magic: go through gazebo model names one by one
   if (!robot_state_inited) {
     if (!ros::param::has("/robot_name")) {
       ROS_ERROR("Failed to read rosparam `/robot_name`");
@@ -122,6 +172,25 @@ void modelStatesCallback(const gazebo_msgs::ModelStatesConstPtr msg) {
     ros::param::get("/robot_bbox_z", robot_height);
     assert(robot_height > 0.0f);
 
+    if (!ros::param::has("/robot_wheel_radius")) {
+      ROS_ERROR("Failed to read rosparam `/robot_wheel_radius`");
+      return;
+    }
+    float robot_wheel_radius = -1.0f;
+    ros::param::get("/robot_wheel_radius", robot_wheel_radius);
+    if (!ros::param::has("/robot_wheel_radius")) {
+      ROS_ERROR("Failed to read rosparam `/robot_wheel_radius`");
+      return;
+    }
+    assert(robot_wheel_radius > 0.0f);
+    float robot_mobility_height_ratio = -1.0f;
+    ros::param::get("/robot_mobility_height_ratio",
+                    robot_mobility_height_ratio);
+    assert(robot_mobility_height_ratio > 0.0f);
+    robot_mobility_max_height =
+        robot_wheel_radius * robot_mobility_height_ratio;
+    assert(robot_mobility_max_height > 0.0f);
+
     for (int i = 0; i < msg->name.size(); ++i) {
       if (msg->name[i] == robot_name) {
         robot_state_index = i;
@@ -130,33 +199,36 @@ void modelStatesCallback(const gazebo_msgs::ModelStatesConstPtr msg) {
     assert(robot_state_index >= 0);
 
     robot_state.model_name = robot_name;
-    robot_state.pose = msg->pose.at(robot_state_index);
-    robot_state.twist = msg->twist.at(robot_state_index);
     robot_state.reference_frame = "/world";
 
-    // position:
-    //   x: 20.000076757763342
-    //   y: 19.99997709945979
-    //   z: 0.13227261495344791
-    // orientation:
-    //   x: 3.2100818533505595e-06
-    //   y: 4.226063762422636e-08
-    //   z: 2.1034668619371185e-06
-    //   w: 0.9999999999926346
+    robot_state_inited = true;
   }
 
   robot_state.pose = msg->pose.at(robot_state_index);
   robot_state.twist = msg->twist.at(robot_state_index);
+
+  Eigen::Isometry3d world__eigenT__base_link;
+  tf::poseMsgToEigen(robot_state.pose, world__eigenT__base_link);
+  world__eigenT__velodyne =
+      world__eigenT__base_link * base_link__eigenT__velodyne;
 }
+
+/*
+  Main function
+*/
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "farmland_mapping_node");
+  ros::NodeHandle nh;
+
   if (!initOccupancyGrid()) {
     ROS_ERROR("Failed to initialize occupancy grid");
     return -1;
   }
-
-  ros::NodeHandle nh;
+  if (!initRobotTF()) {
+    ROS_ERROR("Failed to initialize robot tf");
+    return -1;
+  }
 
   ros::Subscriber velodyne_point_cloud_sub =
       nh.subscribe("/points", 100, velodynePointCloudCallback);
@@ -164,7 +236,7 @@ int main(int argc, char **argv) {
       nh.subscribe("/gazebo/model_states", 100, modelStatesCallback);
 
   ros::Rate loop_rate(100);
-  ROS_INFO("In farmland_mapping_node\n");
+  ROS_INFO("In farmland_mapping_node: looping starts\n");
   while (ros::ok()) {
     ros::spinOnce();
     loop_rate.sleep();
--- src/farmland_simulator/README.md
+++ src/farmland_simulator/README.md
@@ -2,6 +2,8 @@
 
 ## Instructions to Measure Robot Dimensions
 
+### Overall Dimensions
+
 - Edit `/opt/ros/noetic/share/husky_description/urdf/husky.urdf.xacro` line 50
     - change 0 to 1
     - Line 50 becomes `  <xacro:arg name="laser_3d_enabled" default="$(optenv HUSKY_LASER_3D_ENABLED 1)" />`
@@ -18,3 +20,54 @@ With Velodyne VLP-16, the dimensions of Husky are
     - x: 0.9854 m
     - y: 0.6851 m
     - z: 1.08983 m
+
+### Component Dimensions
+
+Read `model.urdf`, look for lines like `<cylinder length="0.1143" radius="0.1651"/>`, from which we know that the radii of wheels are all 0.1651.
+
+
+## Transformations
+
+### world__T__base_link
+
+Gazebo magic returns the absolute pose of the robot `base_link`, i.e. $T_{base_link}^{world}$. Point cloud center is at `velodyne` link. The transformation between these two links is defined in the tf tree.
+
+The default setting in `husky_velodyne_spawn.launch` assigns the robot pose to be:
+```xml
+  <arg name="robot_init_x" default="20.0"/>
+  <arg name="robot_init_y" default="20.0"/>
+  <arg name="robot_init_z" default="0.0"/>
+  <arg name="robot_init_yaw" default="0.0"/>
+```
+, and from Gazebo magic, initially robot pose is
+```yaml
+position:
+  x: 20.000076757763342
+  y: 19.99997709945979
+  z: 0.13227261495344791
+orientation:
+  x: 3.2100818533505595e-06
+  y: 4.226063762422636e-08
+  z: 2.1034668619371185e-06
+  w: 0.9999999999926346
+```
+
+Initial z is not 0, but ~0.13227. That may come from the height of base_link from the robot bottom, verified by the following lines from `model.urdf`.
+
+```xml
+  <!-- Base footprint is on the ground under the robot -->
+  <joint name="base_footprint_joint" type="fixed">
+    <origin rpy="0 0 0" xyz="0 0 -0.13228"/>
+    <parent link="base_link"/>
+    <child link="base_footprint"/>
+  </joint>
+```
+
+### base_link__T__velodyne
+
+```
+ 1      0      0 0.0812
+ 0      1      0      0
+ 0      0      1 0.9127
+ 0      0      0      1
+```
