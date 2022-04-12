7c8e852979e9bf2a98eb417328837e0b6c4f92ca
Calen Robinson <lokera6@gmail.com>
2022-04-12 18:34:32 -0400

    Add constructor and getCommand

--- src/farmland_controller/include/farmland_controller/pure_pursuit.h
+++ src/farmland_controller/include/farmland_controller/pure_pursuit.h
@@ -20,6 +20,33 @@ namespace farmland_controller
     float min_ld;            // Min lookahead distance
     float k_dd;              // Lookahead speed mulitplier.
 
+    PurePursuit():
+      desired_speed(1),
+      goal_dist_epsilon(1.0),
+      max_ld(2),
+      min_ld(0.5),
+      k_dd(1){
+    }
+
+    PurePursuit(float desired_speed, float goal_dist_epsilon,
+                float max_ld, float min_ld, float k_dd):
+      desired_speed(desired_speed),
+      goal_dist_epsilon(goal_dist_epsilon),
+      max_ld(max_ld),
+      min_ld(min_ld),
+      k_dd(k_dd){
+    }
+    /**
+     * @brief Get the twist to control to a path
+     * 
+     * @param robot_pose 
+     * @param path 
+     * @return geometry_msgs::Twist 
+     */
+    std::pair<geometry_msgs::Twist,bool> getCommand(gazebo_msgs::ModelState &robot_state,
+                                  const nav_msgs::Path &path);
+
+  private:
     float euclideanDistance2d(geometry_msgs::Point a, geometry_msgs::Point b);
 
     /**
@@ -52,10 +79,10 @@ namespace farmland_controller
      */
     float getTurningRadius(float dist_to_target_point, float heading_delta);
 
-        /**
-         * Gets the twist (translational velocity and angular velocity) from a turning
-         * radius
-         */
-        geometry_msgs::Twist getTwist(float radius, float target_speed);
+    /**
+     * Gets the twist (translational velocity and angular velocity) from a turning
+     * radius
+     */
+    geometry_msgs::Twist getTwist(float radius, float target_speed);
   };
 }
--- src/farmland_controller/src/pure_pursuit.cpp
+++ src/farmland_controller/src/pure_pursuit.cpp
@@ -1,73 +1,111 @@
 #include <farmland_controller/pure_pursuit.h>
 #define PP PurePursuit
-namespace farmland_controller {
-    float PP::euclideanDistance2d(geometry_msgs::Point a, geometry_msgs::Point b) {
-        return sqrt(pow(a.x-b.x,2)+pow(a.y-b.y,2));
-    }
+namespace farmland_controller
+{
+  std::pair<geometry_msgs::Twist,bool> PP::getCommand(gazebo_msgs::ModelState &robot_state,
+                                  const nav_msgs::Path &path)
+  {
+    float robot_speed; // Translational speed of the robot
+    float ld; // The lookahead distance
+    geometry_msgs::Point tp; // The lookahead point
+    float alpha; // Delta between current robot heading and direction of target point
+    float target_dist; // Distance to target point
+    float turning_radius; // Turning radius the robot needs to get to tp
+    geometry_msgs::Twist cmd_vel; // Command sent to robot
 
-    /**
-     * Extract yaw from a pose
-     */
-    float PP::yawFromPose(geometry_msgs::Pose &pose) {
-        static tf::Quaternion quaternion;
-        static tf::Matrix3x3 rot_matrix;
-        static double roll, pitch, yaw;
+    size_t num_poses = path.poses.size();
+    geometry_msgs::Point goal_point = path.poses[num_poses-1].pose.position;
+    robot_speed = robot_state.twist.linear.x;
 
-        // Get robot state
-        tf::quaternionMsgToTF(pose.orientation, quaternion);
-        rot_matrix.setRotation(quaternion);
-        rot_matrix.getRPY(roll, pitch, yaw);
-        return yaw;
-    }
+    ld = getLookAheadDistance(robot_speed, min_ld, max_ld, k_dd);
+    tp = getTargetPoint(robot_state.pose,path,ld);
+    alpha = getHeadingDelta(robot_state.pose,tp);
+    target_dist = euclideanDistance2d(robot_state.pose.position, tp);
+    turning_radius = getTurningRadius(target_dist, alpha);
+    cmd_vel = getTwist(turning_radius, desired_speed);
 
-    /**
-     * Gets the lookahead distance for the robot
-     */
-    float PP::getLookAheadDistance(float robot_speed, float min_ld, float max_ld,
-                            float k_dd) {
-        float ld = robot_speed * k_dd;
-        if (ld < min_ld) ld = min_ld;
-        if (ld > max_ld) ld = max_ld;
-        return ld;
-    }
+    std::pair<geometry_msgs::Twist,bool> result;
+    result.first = cmd_vel;
 
-    /**
-     * Gets a lookahead point
-     * TODO: Implement function stub
-     */
-    geometry_msgs::Point PP::getTargetPoint(geometry_msgs::Pose &robot_pose,
-                                        const nav_msgs::Path &path, float ld) {
-        geometry_msgs::Point point;
-        return point;
-    }
+    result.second = euclideanDistance2d(robot_state.pose.position, goal_point) < goal_dist_epsilon;
+    return result;
+  }
+  float PP::euclideanDistance2d(geometry_msgs::Point a, geometry_msgs::Point b)
+  {
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+  }
 
-    /**
-     * Gets the heading delta needed to have the robot face the target point
-     * TODO: Implement function stub
-     */
-    float PP::getHeadingDelta(geometry_msgs::Pose &robot_pose,
-                        geometry_msgs::Point target_point) {
-        float yaw = yawFromPose(robot_pose);
-        float delta_y = robot_pose.position.y - target_point.y;
-        float delta_x = robot_pose.position.x - target_point.x;
-        float angleFromRobotToTarget = atan2(delta_y,delta_x);
-        float heading_delta = angleFromRobotToTarget - yaw;
-        return heading_delta;
-    }
+  /**
+   * Extract yaw from a pose
+   */
+  float PP::yawFromPose(geometry_msgs::Pose &pose)
+  {
+    static tf::Quaternion quaternion;
+    static tf::Matrix3x3 rot_matrix;
+    static double roll, pitch, yaw;
 
-    /**
-     * Gets the turning radius needed to reach target point
-     */
-    float PP::getTurningRadius(float dist_to_target_point, float heading_delta) {
-        return 0;
-    }
+    // Get robot state
+    tf::quaternionMsgToTF(pose.orientation, quaternion);
+    rot_matrix.setRotation(quaternion);
+    rot_matrix.getRPY(roll, pitch, yaw);
+    return yaw;
+  }
 
-    /**
-     * Gets the twist (translational velocity and angular velocity) from a turning
-     * radius
-     */
-    geometry_msgs::Twist PP::getTwist(float radius, float target_speed) {
-        geometry_msgs::Twist twist;
-        return twist;
-    }               // eg. ld = clip(k_dd * speed, min_ld, max_ld)
+  /**
+   * Gets the lookahead distance for the robot
+   */
+  float PP::getLookAheadDistance(float robot_speed, float min_ld, float max_ld,
+                                 float k_dd)
+  {
+    float ld = robot_speed * k_dd;
+    if (ld < min_ld)
+      ld = min_ld;
+    if (ld > max_ld)
+      ld = max_ld;
+    return ld;
+  }
+
+  /**
+   * Gets a lookahead point
+   * TODO: Implement function stub
+   */
+  geometry_msgs::Point PP::getTargetPoint(geometry_msgs::Pose &robot_pose,
+                                          const nav_msgs::Path &path, float ld)
+  {
+    geometry_msgs::Point point;
+    return point;
+  }
+
+  /**
+   * Gets the heading delta needed to have the robot face the target point
+   * TODO: Implement function stub
+   */
+  float PP::getHeadingDelta(geometry_msgs::Pose &robot_pose,
+                            geometry_msgs::Point target_point)
+  {
+    float yaw = yawFromPose(robot_pose);
+    float delta_y = robot_pose.position.y - target_point.y;
+    float delta_x = robot_pose.position.x - target_point.x;
+    float angleFromRobotToTarget = atan2(delta_y, delta_x);
+    float heading_delta = angleFromRobotToTarget - yaw;
+    return heading_delta;
+  }
+
+  /**
+   * Gets the turning radius needed to reach target point
+   */
+  float PP::getTurningRadius(float dist_to_target_point, float heading_delta)
+  {
+    return 0;
+  }
+
+  /**
+   * Gets the twist (translational velocity and angular velocity) from a turning
+   * radius
+   */
+  geometry_msgs::Twist PP::getTwist(float radius, float target_speed)
+  {
+    geometry_msgs::Twist twist;
+    return twist;
+  } // eg. ld = clip(k_dd * speed, min_ld, max_ld)
 }
